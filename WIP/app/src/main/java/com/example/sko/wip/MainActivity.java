f61cf9b0d08cc5f115fa125d77dceaf10e2b04a5
SK-Ouchi <sch.skouchi@gmail.com>
2017-04-19 17:06:03 -0700

    Assignment 2

--- WIP/app/src/main/java/com/example/sko/wip/MainActivity.java
+++ WIP/app/src/main/java/com/example/sko/wip/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.sko.wip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,4 +51,17 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
 }
